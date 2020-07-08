512bcd946471e405b9b50ca68933ce1ba140b9ee
Roney <roneyrogerio92@gmail.com>
2020-07-08 07:45:25 -0300

    new function to check and save ceil and floor color

--- ngn_int_config_header.c
+++ ngn_int_config_header.c
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 03:26:57 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/07/07 08:50:48 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/07/08 07:40:43 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ngn_int_config_header(int fd, t_ngn *ngn)
 	char	**splitted_line;
 	int		err;
 
-	configs = 6;
+	configs = 8;
 	while (configs && (splitted_line = ngn_int_splittedline(fd)) != NULL)
 	{
 		err = ngn_int_config_save_header(splitted_line, ngn);
@@ -46,6 +46,9 @@ int	ngn_int_config_save_header(char **splitted_line, t_ngn *ngn)
 			ft_strcmp(splitted_line[0], "EA") == 0 ||
 			ft_strcmp(splitted_line[0], "S") == 0)
 		return (ngn_int_config_save_texture(splitted_line, ngn));
+	else if (ft_strcmp(splitted_line[0], "F") == 0 ||
+			ft_strcmp(splitted_line[0], "C") == 0)
+		return (ngn_int_config_save_color(splitted_line, ngn));
 	return (EXIT_FAILURE);
 }
 
@@ -94,5 +97,34 @@ int	ngn_int_config_save_texture(char **splitted_line, t_ngn *ngn)
 		ngn->texture.ea = bmp;
 	if (ft_strcmp(splitted_line[0], "S") == 0)
 		ngn->texture.s = bmp;
+	return (EXIT_SUCCESS);
+}
+
+int	ngn_int_config_save_color(char **spline, t_ngn *ngn)
+{
+	char		**spcolor;
+	t_argb		rgb;
+
+	if (ft_splitlen(spline) != 2 && (g_ngnerr = NGN_COLERR))
+		return (EXIT_FAILURE);
+	if ((!(spcolor = ft_split(spline[1], ',')) || ft_splitlen(spcolor) != 3 ||
+		!ft_isnumber(spcolor[0]) || !ft_isnumber(spcolor[1]) || !ft_isnumber(
+		spcolor[2]) || ft_atoi(spcolor[0]) < 0 || ft_atoi(spcolor[0]) > 255 ||
+		ft_atoi(spcolor[1]) < 0 || ft_atoi(spcolor[1]) > 255 || ft_atoi(spcolor
+		[2]) < 0 || ft_atoi(spcolor[2]) > 255) && (g_ngnerr = NGN_COLERR))
+	{
+		ft_splitclear(spcolor);
+		return (EXIT_FAILURE);
+	}
+	rgb.r = ft_atoi(spcolor[0]);
+	rgb.g = ft_atoi(spcolor[1]);
+	rgb.b = ft_atoi(spcolor[2]);
+	ngn->ceil.argb.r = spline[0][0] == 'C' ? rgb.r : ngn->ceil.argb.r;
+	ngn->ceil.argb.g = spline[0][0] == 'C' ? rgb.g : ngn->ceil.argb.g;
+	ngn->ceil.argb.b = spline[0][0] == 'C' ? rgb.b : ngn->ceil.argb.b;
+	ngn->floor.argb.r = spline[0][0] == 'F' ? rgb.r : ngn->floor.argb.r;
+	ngn->floor.argb.g = spline[0][0] == 'F' ? rgb.g : ngn->floor.argb.g;
+	ngn->floor.argb.b = spline[0][0] == 'F' ? rgb.b : ngn->floor.argb.b;
+	ft_splitclear(spcolor);
 	return (EXIT_SUCCESS);
 }
