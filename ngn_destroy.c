d67fc6f007083639590446adb340104f7eafc99b
Roney <roneyrogerio92@gmail.com>
2020-07-19 10:17:04 -0300

    Clear matrix

--- ngn_destroy.c
+++ ngn_destroy.c
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 03:48:36 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/07/01 08:47:01 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/07/19 08:50:56 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ void	ngn_destroy(t_ngn *ngn)
 	lbmp_destroy(ngn->texture.we);
 	lbmp_destroy(ngn->texture.ea);
 	lbmp_destroy(ngn->texture.s);
+	ngn_int_matrix_clear(ngn->mx);
 	free(ngn);
 }
